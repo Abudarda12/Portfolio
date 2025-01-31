0d95a5c64d82b28882b4243bd327c274e8db1141
abudarda12 <abudardaansari66@gmail.com>
2025-01-31 22:11:10 +0530

    added hamsberger icom

--- Portfolio/src/Components/Nav.jsx
+++ Portfolio/src/Components/Nav.jsx
@@ -1,11 +1,16 @@
+import React,{useState} from 'react';
 import './Nav.css';
 export default function Nav(){
+    const [isActive,setIsActive]=useState(false);
+    const handleClick=()=>{
+        setIsActive(!isActive);
+    }
     return(
         <div className="nav">
             <div className="nav__logo">
                 <h1>Abudarda Ansari</h1>
             </div>
-            <div className="nav__links">
+            <div className={`nav__links ${isActive ? 'active' : ''}`}>
                 <a href="#about">About</a>
                 <a href="#projects">Projects</a>
                 <a href="#contact">Contact</a>
@@ -14,6 +19,9 @@ export default function Nav(){
                      <button>Download CV</button>
                      </a>
                 </div>
+                <button className="nav__toggle" aria-label="Toggle navigation" onClick={handleClick}>
+                &#9776;
+                </button>
             </div>
             
            
